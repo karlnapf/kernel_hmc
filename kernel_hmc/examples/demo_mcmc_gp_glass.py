cc5b488502d155edb8bfde6ecafaa47b0492996a
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-11-17 12:35:17 +0000

    added kmc to samplers

--- kernel_hmc/examples/demo_mcmc_gp_glass.py
+++ kernel_hmc/examples/demo_mcmc_gp_glass.py
@@ -1,7 +1,9 @@
+from kernel_exp_family.estimators.lite.gaussian import KernelExpLiteGaussianAdaptive
 from kernel_hmc.densities.gaussian import IsotropicZeroMeanGaussian
 from kernel_hmc.examples.demo_mcmc_kmc_static import visualise_trace
 from kernel_hmc.mini_mcmc.mini_mcmc import mini_mcmc
 from kernel_hmc.proposals.base import standard_sqrt_schedule
+from kernel_hmc.proposals.kmc import KMC
 from kernel_hmc.proposals.metropolis import AdaptiveMetropolis,\
     StandardMetropolis, KernelAdaptiveMetropolis
 from kernel_hmc.tools.log import Log
@@ -32,7 +34,6 @@ def get_am_instance(target):
 
 def get_mh_instance(target):
     # adaptive version that tunes itself towards the "optimal" acceptance rate
-    # set schedule=None for completely non-adaptive version
     step_size = 0.7
     schedule = standard_sqrt_schedule
     acc_star = 0.234
@@ -42,8 +43,6 @@ def get_mh_instance(target):
 
 def get_kam_instance(target):
     # adaptive version that tunes itself towards the "optimal" acceptance rate
-    # set schedule=None for completely non-adaptive version
-    
     step_size = 1.
     gamma2 = 0.1
     schedule = standard_sqrt_schedule
@@ -53,7 +52,27 @@ def get_kam_instance(target):
     return KernelAdaptiveMetropolis(target, D, N=200, kernel_sigma=kernel_sigma,
                                     gamma2=gamma2, step_size=step_size,
                                     adaptation_schedule=schedule, acc_star=acc_star)
+
+def get_kmc_instance(target):
+    step_size_min = 0.01
+    step_size_max = 0.1
+    num_steps_min = 1
+    num_steps_max = 10
+    momentum = IsotropicZeroMeanGaussian(D=D)
+    schedule = standard_sqrt_schedule
+    acc_star = 0.7
     
+    # fully automatic parameter tuning in every fit call
+    surrogate = KernelExpLiteGaussianAdaptive(sigma=1., lmbda=.001, D=D, N=200,
+                                        n_initial=3, n_iter=3, minimum_size_learning=100,
+                                        n_initial_relearn=3, n_iter_relearn=3,
+                                        param_bounds={'sigma': [-3, 3]}
+                                        )
+    
+    return KMC(surrogate, target,
+               momentum, num_steps_min, num_steps_max, step_size_min, step_size_max,
+               adaptation_schedule=schedule, acc_star=acc_star)
+
 if __name__ == '__main__':
     """
     This example samples from the marginal posterior over hyper-parameters of a
@@ -76,7 +95,8 @@ if __name__ == '__main__':
     samplers = [
                 get_am_instance(target),
                 get_mh_instance(target),
-                get_kam_instance(target)
+                get_kam_instance(target),
+                get_kmc_instance(target)
                ]
     
     for sampler in samplers:
